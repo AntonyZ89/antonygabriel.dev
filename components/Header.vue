7bc7f340fcb11249e66d74842d052549f5ed7509
AntonyZ89 <antonyz.dev@gmail.com>
2025-02-20 17:33:45 -0300

    chore: add profile image

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="mb-8 border-l-4 border-nvim-green pl-4">
-    <h1 class="text-3xl mb-2">
-      :AntonyDev
-    </h1>
-    <div class="text-nvim-blue">
-      > Bem-vindo ao meu ambiente de desenvolvimento profissional <CursorBlink />
+  <header class="flex flex-col gap-y-4 justify-end border-l-4 border-nvim-green pl-4 mb-8">
+    <div>
+      <NuxtImg
+        src="/images/profile.jpeg"
+        alt="Perfil"
+        class="rounded-full"
+        sizes="200px"
+      />
     </div>
-  </div>
+
+    <div>
+      <h1 class="text-3xl mb-2">
+        :AntonyDev
+      </h1>
+      <div class="text-nvim-blue">
+        > Bem-vindo ao meu ambiente de desenvolvimento profissional <CursorBlink />
+      </div>
+    </div>
+  </header>
 </template>
